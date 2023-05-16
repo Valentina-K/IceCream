17c17aa45b1c38a4126a5f7cdde1ebb5af1abf13
Valentшna-K <kvalentyna@hotmail.com>
2023-05-16 14:57:29 +0300

    made gallery, but dont't like

--- js/gallery.js
+++ js/gallery.js
@@ -1,13 +1,18 @@
 (()=>{
-    const imgArray = document.querySelectorAll('.gelleryImg');
+    const imgArray = document.querySelectorAll('.gallery-img');
+    
 
     imgArray.forEach(img=>img.addEventListener('animationiteration', animationImg));
-
+    imgArray[0].classList.add('active');
     let index=0;
     function animationImg(e){
-        index = Number(e.target.id);
-        imgArray.forEach(item=>{
-            item.classList.toggle('active')
-        });
+        console.log(e)
+        imgArray[index].classList.toggle('active')
+        if(index!=2){
+            index = index+1;
+        } else {
+            index=0;
+        }
+        imgArray[index].classList.toggle('active');        
     }
 })();
