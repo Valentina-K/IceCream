6e0a9bbed892e0bc2007f6745707262125d5ff8a
Valentшna-K <kvalentyna@hotmail.com>
2023-05-18 15:48:57 +0300

    made buy now modal window

--- js/modal-buy.js
+++ js/modal-buy.js
@@ -2,11 +2,13 @@
     const closeModalBtn = document.querySelector("[buy-modal-close]");
     const openModalBtn = document.querySelector("[buy-modal-open]")
     const modal_buy = document.querySelector("[data-madal-buy]");
+    const menu = document.querySelector('[data-menu]')
 
     openModalBtn.addEventListener("click", toggleModal);
     closeModalBtn.addEventListener("click", toggleModal);
 
     function toggleModal() {
         modal_buy.classList.toggle("is-hidden");
+        menu.classList.toggle('is-open');
     }
 })();
