d66bf1041b103ea95609b9b959132d3b0d0423b3
Valentшna-K <kvalentyna@hotmail.com>
2023-05-29 13:24:38 +0300

    fixed about section, made products section

--- js/modal-buy.js
+++ js/modal-buy.js
@@ -2,13 +2,11 @@
     const closeModalBtn = document.querySelector("[buy-modal-close]");
     const openModalBtn = document.querySelectorAll("[buy-modal-open]")
     const modal_buy = document.querySelector("[data-madal-buy]");
-    const menu = document.querySelector('[data-menu]')
 
     openModalBtn.forEach(button=>button.addEventListener("click", toggleModal));
     closeModalBtn.addEventListener("click", toggleModal);
 
     function toggleModal() {
         modal_buy.classList.toggle("is-hidden");
-        menu.classList.toggle('is-open');
     }
 })();
