f585e63d0d40317bf6609f8e774d61fde6ef088e
Valentшna-K <kvalentyna@hotmail.com>
2023-05-30 15:19:51 +0300

    finished

--- js/gallery.js
+++ js/gallery.js
@@ -1,12 +1,8 @@
 (()=>{
     const imgArray = document.querySelectorAll('.gallery-img');
     
-
     imgArray.forEach(img=>img.addEventListener('animationiteration', animationImg));    
-   
-
     imgArray[0].classList.add('active'); 
-
        
     let index=0;
     function animationImg(){
--- js/sliger.js
+++ js/sliger.js
@@ -1,8 +1,6 @@
 const items = document.querySelectorAll('.gallery');
 const itemCount = items.length;
-const nav = document.querySelectorAll('.dots')/* 
-const nextItem = document.querySelector('.next');
-const previousItem = document.querySelector('.previous'); */
+const nav = document.querySelectorAll('.dots');
 let index = 0;
 nav.forEach(navItem => navItem.addEventListener('click', showItem));
 function showItem(e) {
@@ -13,45 +11,3 @@ function showItem(e) {
   nav[index].classList.add('active');
 }
 
-/* nav.map(navItem => {
-  console.log(navItem)
-  navItem.addEventListener('click', showItem)
-}); */
-
-/* function showNextItem() {
-  items[index].classList.remove('active');
-
-  if(index < itemCount - 1) {
-    index++;
-  } else {
-    index = 0;
-  }
-
-  items[index].classList.add('active');
-}
-
-function showPreviousItem() {
-  items[index].classList.remove('active');
-
-  if(index > 0) {
-    index--;
-  } else {
-    index = itemCount - 1;
-  }
-
-  items[index].classList.add('active');
-} */
-
-/* function keyPress(e) {
-  e = e || window.event;
-  
-  if (e.keyCode == '37') {
-    showPreviousItem();
-  } else if (e.keyCode == '39') {
-    showNextItem();
-  }
-} */
-
-/* nextItem.addEventListener('click', showNextItem);
-previousItem.addEventListener('click', showPreviousItem);
-document.addEventListener('keydown', keyPress); */
